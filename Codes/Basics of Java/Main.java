d8261de2796297f422ddad3ff63d5ebac9d377a6
Akshita Dixit <akshita15dec@gmail.com>
2025-07-22 16:23:16 +0530

    Update Main.java

--- Codes/Basics of Java/Main.java
+++ Codes/Basics of Java/Main.java
@@ -1,4 +1,8 @@
 package Akshita;
 
 public class Main {
+   public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextLine());
+    }
 }
